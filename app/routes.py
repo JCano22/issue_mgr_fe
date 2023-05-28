fd407b92d2183e137f3a4fe269ecc9e2f60d6d4f
Jorge Cano <coronero@gmail.com>
2023-05-27 21:20:44 -0700

    FSDI-111, modified some CSS and finished styling task template.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from flask import (
-    Flask, 
-    render_template, 
+    Flask,
+    render_template,
     request
-    )
+)
 from datetime import datetime
-import requests 
+import requests
 
 app = Flask(__name__)
 
@@ -13,31 +13,35 @@ BACKEND_URL = 'http://127.0.0.1:5000'
 
 @app.get("/")
 def home():
-    mylist = [1, 2, 3, 4 , 5,]
+    mylist = [1, 2, 3, 4, 5,]
     timestamp = datetime.now().strftime("%H:%M:%S")
     return render_template("home.html", ts=timestamp, numbers=mylist)
+
 
 @app.get("/about")
 def about():
     return render_template("about.html")
 
+
 @app.get("/tasks")
 def list_tasks():
     url = "%s/tasks" % BACKEND_URL
     response = requests.get(url)
-    if response.status_code==200:
+    if response.status_code == 200:
         task_list = response.json().get("tasks")
         return render_template("task_list.html", tasks=task_list)
     return render_template("error.html", err=response.status_code), response.status_code
+
 
 @app.get("/tasks/edit/<int:pk>")
 def render_edit_form(pk):
     url = "%s/tasks/%s" % (BACKEND_URL, pk)
     response = requests.get(url)
     if response.status_code == 200:
-        task_data=response.json().get("task")
+        task_data = response.json().get("task")
         return render_template("edit.html", task=task_data)
     return render_template("error.html", err=response.status_code), response.status_code
+
 
 @app.post("/tasks/edit/<int:pk>")
 def update_task(pk):
@@ -48,9 +52,11 @@ def update_task(pk):
         return render_template("success.html")
     return render_template("error.html", err=response.status_code), response.status_code
 
+
 @app.get("/tasks/new")
 def render_new_form():
     return render_template("new.html")
+
 
 @app.post("/task/new")
 def create_task():
@@ -61,6 +67,7 @@ def create_task():
         return render_template("success.html")
     return render_template("error.html", err=response.status_code), response.status_code
 
+
 @app.get("/tasks/<int:pk>")
 def view_task_by_id(pk):
     url = "%s/tasks/%s" % (BACKEND_URL, pk)
@@ -70,4 +77,3 @@ def view_task_by_id(pk):
         return render_template("detail.html", task=task_data)
     return render_template(
         "error.html", err=response.status_code), response.status_code
-    
